f585ac54bc7cc8b41172766add656c80d4064711
Nick Buraglio <buraglio@forwardingplane.net>
2025-10-03 21:13:34 +0000

    Bah, syscall stuff is all different on MacOS than it is on Liinux. Add OS specific checks. This is gross but I don't know how to do it otherwise

---
+++ select_linux.go
@@ -0,0 +1,14 @@
+//go:build linux
+
+package main
+
+import "syscall"
+
+// selectWithTimeout performs a select call and returns whether the fd is ready
+func selectWithTimeout(fd int, fdSet *syscall.FdSet, tv *syscall.Timeval) (bool, error) {
+	n, err := syscall.Select(fd+1, fdSet, nil, nil, tv)
+	if err != nil {
+		return false, err
+	}
+	return n > 0, nil
+}
